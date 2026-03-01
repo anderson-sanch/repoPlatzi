f4de33355af0c618e9a3b35b2315dd0222a423c7
anderson sanchez <anderson.asm201@gmail.com>
2026-02-28 22:28:36 -0500

    class

---
+++ typescript-poo/src/01-class.ts
@@ -0,0 +1,27 @@
+const date = new Date();
+date.getDate();
+date.getDay();
+date.getFullYear();
+
+const date2 = new Date(2020, 11, 25);
+
+console.log(date, date2);
+
+// clase
+
+class MyDate{
+    year: number;
+    month:number;
+    day: number;
+
+    constructor(year: number, month: number, day: number){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
+
+const mydate = new MyDate(2020, 11, 25);
+
+console.log(mydate);
+
