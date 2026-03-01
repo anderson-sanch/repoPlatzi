901860efa57cce22b96fcf6dec40634d09c87803
anderson sanchez <anderson.asm201@gmail.com>
2026-03-01 11:53:19 -0500

    protected

---
+++ typescript-poo/src/09-protected.ts
@@ -0,0 +1,48 @@
+export class Animal {
+    constructor(
+    protected _name:string,
+
+){}
+    move(){
+        console.log('I am moving');
+    }
+
+    gretting(){
+        console.log(`Hi, i'm ${this._name}`);
+        
+    }
+
+    protected doSomething(){
+        console.log('Doing something');
+    }
+     
+}
+
+class Dog extends Animal {
+
+    
+    constructor(
+        name:string,
+        public owner:string
+    ){
+        super(name);
+    }
+
+    bark(times: number) : void {
+        for(let i = 0; i < times; i++){
+            console.log('Woof!' + this._name);
+        }
+        this.doSomething();
+    }
+
+    move(){
+        console.log('I am running as a dog');
+        super.move();
+    }
+}
+
+const thor = new Dog('Thor','Anderson');
+// thor._name = 'Thor II';
+
+thor.bark(2);
+thor.move();
