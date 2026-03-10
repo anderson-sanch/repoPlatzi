29181290128cf9833c68ec14e91882f0552a4171
anderson <correo@correo.com>
2026-03-09 20:59:23 -0500

    node avanzado, singleton y typescript

---
+++ nodea/src/01-singleton.ts
@@ -0,0 +1,25 @@
+class Singleton {
+    static #instance: Singleton
+    private name?:string;
+
+    private constructor(){}
+
+    public static get instance(): Singleton{
+        if(!Singleton.#instance){
+            Singleton.#instance = new Singleton();
+        }
+        return Singleton.#instance
+    }
+    setName (name:string){
+        this.name = name
+    }
+}
+
+const singleton1 = Singleton.instance;
+const singleton2 = Singleton.instance;
+
+if(singleton1 === singleton2){
+    console.log('singleton funciona');
+}else{
+    console.log('singleton fallo');
+}
